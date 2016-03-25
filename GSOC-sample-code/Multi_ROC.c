22e9c6d891e5d7675f15a7da5c98faa0cd6a37c6
Dominic Mills <dominic.millz27@gmail.com>
2016-03-25 11:58:56 -0500

    Made drastically needed changes

--- GSOC-sample-code/Multi_ROC.c
+++ GSOC-sample-code/Multi_ROC.c
@@ -1,75 +1,77 @@
-#ifndef Multi_ROC
-#define Multi_ROC
+#include <TFile.h>
+#include <TH1.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
 
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-#include "TFile.h"
-#include "TGraph.h"
-#include "TCanvas.h"
-#include "TColor.h"
-#include "gStyle.h"
-#include "TLegend.h"
-#include "TLatex.h"
-#include "TH1F.h"
-#include "TTree.h"
-#include "TH2F.h"
+void Multi_ROC(TFile* file){
+    
+    //Create vectors to store the Background values          
+    std::vector<float>* Var0Bkg;
+    std::vector<float>* Var1Bkg;
+    std::vector<float>* Var2Bkg;
+   
+    //Create vectors to store the Signal values
+    std::vector<float>* Var0Sig;
+    std::vector<float>* Var1Sig;
+    std::vector<float>* Var2Sig;
+    
+    //Assumes trees are named 'MyMCBkg' and 'MyMCSig'
+    TTree* Background = (TTree*) file.Get("MyMCBkg"); //Get the Background signal tree from the file
+    TTree* Signal = (TTree*) file.Get("MyMCSig"); //Get the Background signal tree from the file
+    
+    // Links the local variables to the tree branches. TTreeReader could have also been used to do this job.
+    Background->SetBranchAddress("Var0Bkg", &Var0Bkg);
+    Background->SetBranchAddress("Var1Bkg", &Var1Bkg);
+    Background->SetBranchAddress("Var2Bkg", &Var2Bkg);
+    // After this stage the local variables are still empty.
+    // With every subsequent call to myTree->GetEntry(i) the local variables will be filled with the content of branches
+    
+    //Does the same as above except with the Signal branches instead
+    Signal->SetBranchAddress("Var0Sig", &Var0Sig);
+    Signal->SetBranchAddress("Var1Sig", &Var1Sig);
+    Signal->SetBranchAddress("Var2Sig", &Var2Sig);
+    
+    // Loops over all entries in the Background tree
+    int nEntries1 = Background->GetEntries(); // Get the number of entries in this tree
+    for (int iEnt = 0; iEnt < nEntries1; iEnt++) {
+        Background->GetEntry(iEnt); // Gets the next entry (filling the linked variables)
+    }
+    
+    // Loops over all entries in the Signal tree
+    int nEntries2 = Signal->GetEntries(); // Get the number of entries in this tree
+    for (int iEnt = 0; iEnt < nEntries2; iEnt++) {
+        Signal->GetEntry(iEnt); // Gets the next entry (filling the linked variables)
+    }
+    
+        TCanvas *c1 = new TCanvas("c1","ROC Curve",700,500);
+        TMultiGraph *mg = new TMultiGraph();
+        
+        //Create first ROC curve
+        TGraph *gr1 = new TGraph(nEntries1,Var0Bkg,Var0Sig); //signal:  y-axis; background: x-axis
+        gr1->SetLineColor(4);
+        
+        mg->Add(gr1);
+        
+        //Create second ROC curve
+        TGraph *gr2 = new TGraph(nEntries1,Var1Bkg,Var1Sig); //signal:  y-axis; background: x-axis
+        gr1->SetLineColor(3);
+        
+        mg->Add(gr2);
+        
+        //Create third ROC curve
+        TGraph *gr3 = new TGraph(nEntries1,Var2Bkg,Var2Sig); //signal:  y-axis; background: x-axis
+        gr1->SetLineColor(2);
+        
+        mg->Add(gr3);
+        
+        return c1;
+        
+        c1->Print("ROC.png");
+        
+        file1.Close();
 
-void Multi_ROC(){
-    
-    gStyle->SetOptStat(0);
-    gStyle->SetLabelSize(0.08,"xy");
-    gStyle->SetTitleSize(0.08, "xy");
-    gStyle->SetTitleOffset(1.2, "xy");
-    gStyle->SetPadTopMargin(0.1);
-    gStyle->SetPadRightMargin(0.1);
-    gStyle->SetPadBottomMargin(0.16);
-    gStyle->SetPadLeftMargin(0.12);
-    
-    TFile* f = new TFile("mydataset.root");
-    
-    TCanvas *c1 = new TCanvas("c1", "c1", 800, 800);
-    canvas->SetTickx();
-    canvas->SetTicky();
-    
-    //Need to fix this section
-    TH1F h1 = file1.Get("Var0Sig");
-    TH1F h2 = file1.Get("Var1Sig");
-    TH1F h3 = file1.Get("Var2Sig");
-     
-    h1->SetTitle('');
-    h1->GetYaxis().SetTitle('Background rejection');
-    h1->GetXaxis().SetTitle('Signal efficiency');
-    h1->SetLineColor(TColor.kAzure);
-    h1->SetLineWidth(2);
-    h1->SetFillStyle(0);
-    h1->SetFillColor(TColor.kAzure-2);
-     
-    h2->SetLineColor(TColor.kRed);
-    h2->SetLineWidth(2);
-    h2->SetFillStyle(0);
-    
-    h3->SetLineColor(TColor.kGreen);
-    h3->SetLineWidth(2);
-    h3->SetFillStyle(0);
-     
-    l = new TLegend(0.2, 0.25, 0.4, 0.45);
-    l->SetTextSize(0.04);
-    l->SetTextFont(42);
-    l->SetFillColor(10);
-    l->SetLineColor(10);
-    l->SetBorderSize(0);
-    l->AddEntry(h1,"5 trees","l");
-    l->AddEntry(h2,"850 trees","l");
-    l->AddEntry(h3,"2500 trees","l");
-     
-    h1->Draw("HIST");
-    h2->Draw("HISTsame");
-    h3->Draw("HISTsame");
-    l->Draw("same");
-    canvas->RedrawAxis();
-    canvas->Print("ROC.png");
-     
-    file1.Close()
-    
 }
